8ef0e95cca83dc3c45b335366ca309168743b5b1
cam <cam.t.watson@gmail.com>
2024-09-27 18:04:21 -0400

    init commit

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := hello("cam")
-	want := "Hello, cam"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := hello("cam")
+		want := "Hello, cam"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("say 'Hello, world' when empty string", func(t *testing.T) {
+		got := hello("")
+		want := "Hello, world"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
