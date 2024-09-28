f82dc2d5be3ba6112b733ebe909719cf75f9fd1d
cam <cam.t.watson@gmail.com>
2024-09-28 08:29:50 -0400

    helper functions

--- hello_test.go
+++ hello_test.go
@@ -6,17 +6,17 @@ func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := hello("cam")
 		want := "Hello, cam"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, world' when empty string", func(t *testing.T) {
 		got := hello("")
 		want := "Hello, world"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
